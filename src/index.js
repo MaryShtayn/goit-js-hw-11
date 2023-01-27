2acc5093c058ce87bc2dfe0230f4c93667067003
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-01-27 13:59:53 +0100

    updeted js with sync/await

--- src/index.js
+++ src/index.js
@@ -14,45 +14,44 @@ let totalHits = 0;
 form.addEventListener('submit', onSubmit);
 loadBtn.addEventListener('click', onLoad);
 
-function onSubmit(evt) {
-  evt.preventDefault();
-  loadBtn.classList.add('.is_hidden');
-  const searchQuery = evt.currentTarget.elements.searchQuery.value;
-  evt.target.reset();
-  if (searchQuery === '') {
-    Notiflix.Notify.failure('The field must be filled');
-    return;
-  }
+async function onSubmit(evt) {
+  try {
+    evt.preventDefault();
+    const searchQuery = evt.currentTarget.elements.searchQuery.value;
+    evt.target.reset();
 
-  async function getPhotos(page = 1, value = searchQuery) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY_API = '32999869-250accc55f8619ccb56097a0b';
-    const searchParam = new URLSearchParams({
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: 40,
-    });
-    const response = await axios.get(
-      `${BASE_URL}?key=${KEY_API}&q=${value}&page=${page}&${searchParam}`
-    );
-    return response.data;
-  }
+    if (searchQuery === '') {
+      Notiflix.Notify.failure('The field must be filled');
+      return;
+    }
 
-  getPhotos()
-    .then(data => {
-      if (data.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      createMarkup(data.hits);
-      // if (page < data.total)
-      loadBtn.classList.remove('.is_hidden');
-    })
-    .catch(err => Notiflix.Notify.failure('Sorry, something went wrong'));
-  loadBtn.classList.add('.is_hidden');
+    const data = await getPhotos();
+    if (data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    createMarkup(data.hits);
+    loadBtn.classList.remove('is_hidden');
+  } catch (err) {
+    Notiflix.Notify.failure('Sorry, something went wrong');
+  }
+}
+
+async function getPhotos(page = 1, value = searchQuery) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY_API = '32999869-250accc55f8619ccb56097a0b';
+  const searchParam = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+  });
+  const response = await axios.get(
+    `${BASE_URL}?key=${KEY_API}&q=${value}&page=${page}&${searchParam}`
+  );
+  return response.data;
 }
 
 function createMarkup(arr) {
@@ -66,8 +65,7 @@ function createMarkup(arr) {
         views,
         comments,
         downloads,
-      }) =>
-        `<a class="gallery__link" href="${largeImageURL}">  
+      }) => `<a class="gallery__link" href="${largeImageURL}">  
   <img class="gallery__img" src="${webformatURL}" alt="${tags}" loading="lazy"></img>
     <div class="info">
       <p class="info__item"><b>Likes</b><br/>${likes}</p>
@@ -88,26 +86,29 @@ function createMarkup(arr) {
     }
   }
   const lightbox = new SimpleLightbox('.gallery__link', {
-    // captionsData: 'alt',
-    // captionDelay: 250,
+    captionDelay: 250,
   });
   lightbox.on('show.simplelightbox', function () {
     gallery.addEventListener('click', onOpenModal);
   });
 }
 
-function onLoad() {
-  page += 1;
-  getPhotos(page)
-    .then(data => {
-      createMarkup(data.hits);
-      gallery.refresh();
-      if (data.hits === data.totalHits) {
-      }
+async function onLoad() {
+  try {
+    page += 1;
+    const data = await getPhotos();
+    if (per_page * page >= data.totalHits) {
       Notiflix.Notify.failure(
         "We're sorry, but you've reached the end of search results."
       );
-      loadBtn.hidden = true;
-    })
-    .catch(err => console.log(err));
+      return;
+    }
+    createMarkup(data.hits);
+    gallery.refresh();
+    if (data.hits === data.totalHits) {
+      loadBtn.classList.add('is_hidden');
+    }
+  } catch (err) {
+    console.log('Error');
+  }
 }
